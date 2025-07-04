977bb7a590c0c1e945a1b8625039a595c2b7f88d
Millan <43229006+itsmillanessa@users.noreply.github.com>
2025-07-04 12:50:33 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    host: 'dev.millanza.com',
+    user: 'admin',
+    password: 'Millanza2024!'
+  },
+  aws: {
+    accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+    secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+  },
+  secretKey: 'supersecretmillanza'
+};
